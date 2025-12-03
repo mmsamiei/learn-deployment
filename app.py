17ae581c095b029e286b73ce02b7f3c72b9a776b
Mahdi Samiei <mohammadmahdi.samiei@divar.ir>
2025-12-03 16:17:17 +0330

    Add Redis integration and update requirements
    - Implemented Redis connection in app.py to track visitor counts.
    - Added error handling for database connection issues.
    - Updated requirements.txt to include redis and async-timeout packages.

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-# app.py
 from flask import Flask
 import socket
+import os
+import redis
 
 app = Flask(__name__)
 
+# CONFIGURATION:
+# If 'REDIS_HOST' is set in the environment, use it. Otherwise, assume 'localhost'.
+redis_host = os.getenv('REDIS_HOST', 'localhost')
+
+# Connect to the Database
+# (If the DB isn't running, this checks connection but won't crash immediately)
+db = redis.Redis(host=redis_host, port=6379, decode_responses=True)
+
 @app.route('/')
 def hello():
-    # This gets the name of the Container (or Laptop)
     container_id = socket.gethostname()
-    return f"👋 Hello from Python! Running inside ID: {container_id}\n"
+    
+    try:
+        # Ask Database to increment the "visits" counter
+        visits = db.incr('counter')
+    except redis.exceptions.ConnectionError:
+        visits = "<i>[Cannot connect to Database]</i>"
+
+    return (
+        f"👋 Hello! Running inside ID: {container_id}<br>"
+        f"📈 Visitor Number: <b>{visits}</b>\n"
+    )
 
 if __name__ == "__main__":
-    # host='0.0.0.0' is CRITICAL for Docker
-    # It means "Listen to connections from outside this computer"
     app.run(host='0.0.0.0', port=5000)
